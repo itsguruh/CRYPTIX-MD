6045d170b362b3ff26aa78d51a78a8bff8b898c9
GURU <cryptixmd@gmail.com>
2025-09-12 02:23:55 +0300

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -4,7 +4,7 @@ module.exports = {
   AUTO_REACT: process.env.AUTO_REACT || 'false',
   LOG_LEVEL: process.env.LOG_LEVEL || 'info',
   PORT: process.env.PORT || 3000,
-  WELCOME_MSG: process.env.WELCOME_MSG || `*Hi CRYPTIX‑MD User! 👋🏻*\nSimple, Straightforward, But Loaded With Features 🎊\nThanks for using CRYPTIX‑MD 🫂`,
-  SOURCE_URL: process.env.SOURCE_URL || 'https://github.com/itsguruh/CRYPTIX-MD',
+  WELCOME_MSG: process.env.WELCOME_MSG || '*Hi CRYPTIX‑MD User! 👋🏻*\\nSimple, Straightforward, But Loaded With Features 🎊\\nThanks for using CRYPTIX‑MD 🫂',
+  SOURCE_URL: process.env.SOURCE_URL || 'https://github.com/itsguruh/CRYPTIX‑MD',
   OWNER_NUMBER: process.env.OWNER_NUMBER || ''
 };
