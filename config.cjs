c22614b82afa62ec458657791c06f436fd4c5fa8
GURU <cryptixmd@gmail.com>
2025-09-12 02:02:35 +0300

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -1,10 +1,10 @@
-export default {
+module.exports = {
   PREFIX: process.env.PREFIX || '!',
   SESSION_ID: process.env.SESSION_ID || '',
   AUTO_REACT: process.env.AUTO_REACT || 'false',
   LOG_LEVEL: process.env.LOG_LEVEL || 'info',
   PORT: process.env.PORT || 3000,
-  WELCOME_MSG: process.env.WELCOME_MSG || '*Hi CRYPTIX-MD User! 👋🏻*\nSimple, Straightforward, But Loaded With Features 🎊\nThanks for using CRYPTIX-MD 🫂',
+  WELCOME_MSG: process.env.WELCOME_MSG || `*Hi CRYPTIX‑MD User! 👋🏻*\nSimple, Straightforward, But Loaded With Features 🎊\nThanks for using CRYPTIX‑MD 🫂`,
   SOURCE_URL: process.env.SOURCE_URL || 'https://github.com/itsguruh/CRYPTIX-MD',
-  OWNER_NUMBER: process.env.OWNER_NUMBER || '',  // your WhatsApp number, e.g. 2547XXXXXXXX
+  OWNER_NUMBER: process.env.OWNER_NUMBER || ''
 };
